1a4b5d23f58548d04cb32baf06f24d9371ea870c
Gabor Varga <gabor.varga@sigmatechnology.se>
2019-05-30 19:18:37 +0200

    final version

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -7,14 +7,22 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['../home/home.component.css', './video.component.css']
 })
 export class VideoComponent implements OnInit {
-
   videoId: number;
   videoSource: string;
+  currentTime: number;
+  stopIndex = 0;
+  video: HTMLVideoElement;
+  overViewTimeStops = [2.43835, 30.566];
+  wagonGuardTimeStops = [3.033, 10.0, 20.159, 22.744];
+  cbmTimeStops = [1.15, 6.339, 28.810, 68.255];
+  automationTimeStops = [3.067, 27.317, 30.175, 41.608];
+
   @ViewChild('videoPlayer') videoplayer: ElementRef;
   constructor(private route: ActivatedRoute,
               private router: Router) {}
 
   ngOnInit() {
+    this.video = this.videoplayer.nativeElement;
     const id = this.route.snapshot.paramMap.get('id');
     this.videoId = +id;
     this.loadVideoById();
@@ -26,22 +34,22 @@ export class VideoComponent implements OnInit {
     }
     switch (this.videoId) {
       case 1: {
-        this.videoSource = './assets/Overview_FULL.mp4';
+        this.videoSource = './assets/Overview.mp4';
         this.openFullScreen();
         break;
       }
       case 2: {
-        this.videoSource = './assets/Wagon Guard_FULL.mp4';
+        this.videoSource = './assets/Wagon Guard.mp4';
         this.openFullScreen();
         break;
       }
       case 3: {
-        this.videoSource = './assets/CBM_FULL.mp4';
+        this.videoSource = './assets/CBM.mp4';
         this.openFullScreen();
         break;
       }
       case 4: {
-        this.videoSource = './assets/Automation_FULL.mp4';
+        this.videoSource = './assets/Automation.mp4';
         this.openFullScreen();
         break;
       }
@@ -51,6 +59,40 @@ export class VideoComponent implements OnInit {
     }
   }
 
+  onTimeUpdate(value: any): void {
+    this.currentTime = value.target.currentTime;
+    console.log(this.currentTime);
+    switch (this.videoId) {
+      case 1: {
+        if (this.currentTime >= this.overViewTimeStops[this.stopIndex]) {
+          this.video.pause();
+          this.stopIndex++;
+        }
+        break;
+      }
+      case 2: {
+        if (this.currentTime >= this.wagonGuardTimeStops[this.stopIndex]) {
+          this.video.pause();
+          this.stopIndex++;
+        }
+        break;
+      }
+      case 3: {
+        if (this.currentTime >= this.cbmTimeStops[this.stopIndex]) {
+          this.video.pause();
+          this.stopIndex++;
+        }
+        break;
+      }
+      case 4: {
+        if (this.currentTime >= this.automationTimeStops[this.stopIndex]) {
+          this.video.pause();
+          this.stopIndex++;
+        }
+        break;
+      }
+    }
+  }
 
   onVideoEnded() {
     this.navToHome();
@@ -60,28 +102,34 @@ export class VideoComponent implements OnInit {
     this.router.navigateByUrl('/');
   }
 
-  onTimeUpdate(value) {
-    console.log(value.target.currentTime);
-  }
-
-  // toggleVideo(event: any): void {
-    //   let video: HTMLVideoElement;
-    //   video = this.videoplayer.nativeElement;
-  //   video.paused ? video.play() : video.pause();
-  // }
-
   navToPreviousPage(): void {
     const tempId = this.videoId - 1;
-    if (tempId !== 0) {
+    if ((!this.video.ended) && (this.overViewTimeStops.length >= this.stopIndex) ||
+      (!this.video.ended) && (this.wagonGuardTimeStops.length >= this.stopIndex) ||
+      (!this.video.ended) && (this.cbmTimeStops.length >= this.stopIndex) ||
+      (!this.video.ended) && (this.automationTimeStops.length >= this.stopIndex)) {
+      this.video.currentTime = 0;
+      this.video.play();
+    } else if (tempId !== 0) {
+      this.stopIndex = 0;
       this.router.navigate(['/video', (tempId)]);
       this.loadVideoById(tempId);
     } else {
+      this.stopIndex = 0;
       this.router.navigateByUrl('/');
     }
   }
 
   navToNextPage(): void {
-    if (this.videoId < 4) {
+    if ((!this.video.ended) && (this.overViewTimeStops.length >= this.stopIndex) ||
+        (!this.video.ended) && (this.wagonGuardTimeStops.length >= this.stopIndex) ||
+        (!this.video.ended) && (this.cbmTimeStops.length >= this.stopIndex) ||
+        (!this.video.ended) && (this.automationTimeStops.length >= this.stopIndex)) {
+      this.video.currentTime = this.currentTime;
+      // this.video.currentTime = 22;
+      this.video.play();
+    } else if (this.videoId < 4) {
+      this.stopIndex = 0;
       const tempId = this.videoId + 1;
       this.router.navigate(['/video', (tempId)]);
       this.loadVideoById(tempId);
@@ -109,28 +157,3 @@ export class VideoComponent implements OnInit {
   }
 }
 
-// playVideoParts(type: string, actPart: number): void {
-//   this.videoPart = actPart;
-//   this.videoSource = './assets/' + type + '_' + this.videoPart + '.mp4';
-// }
-
-// navToPreviousPart(videoPart: any): void {
-//   videoPart--;
-//   if (videoPart < 1) {
-//     this.videoId--;
-//     this.loadVideoById();
-//   } else {
-//     this.playVideoParts(this.videoName, videoPart.toString());
-//   }
-// }
-
-// navToNextPart(videoPart: any): void {
-//   videoPart++;
-//   if (videoPart > this.nrOfVideoParts) {
-//     this.videoId++;
-//     this.loadVideoById();
-//     // this.navToHome();
-//   } else {
-//     this.playVideoParts(this.videoName, videoPart.toString());
-//   }
-// }
